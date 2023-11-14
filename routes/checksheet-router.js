a176f5b97a983031d61c652713c6c147c14a012d
EnvyL27 <farrel.ahmad.zaidan@gmail.com>
2023-11-14 16:46:26 +0700

    update

--- routes/checksheet-router.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var bodyParser = require("body-parser");
-
-
-module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,55 +1,55 @@
 var express = require('express');
 var router = express.Router();
 var multer = require('multer');
+// app.use(
+//   fileUpload({
+//       limits: {
+//           fileSize: 10000000,
+//       },
+//       abortOnLimit: true,
+//   })
+// );
 
-var express = require('express')
-var multer  = require('multer')
-var port = 3843;
+// app.use(express.static('public'));
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  // res.render('index', { title: 'Express' });
+  res.send('Hello World!')
+});
 
-var app = express()
+const upload = multer({
+  dest: "../images"
+  // you might also want to set some limits: https://github.com/expressjs/multer#limits
+});
 
-var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './images')
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname)
+router.post(
+  "/upload",
+  upload.single("file" /* name attribute of <file> element in your form */),
+  (req, res) => {
+    const tempPath = req.file.path;
+    const targetPath = path.join(__dirname, "./uploads/image.png");
+
+    if (path.extname(req.file.originalname).toLowerCase() === ".png") {
+      fs.rename(tempPath, targetPath, err => {
+        if (err) return handleError(err, res);
+
+        res
+          .status(200)
+          .contentType("text/plain")
+          .end("File uploaded!");
+      });
+    } else {
+      fs.unlink(tempPath, err => {
+        if (err) return handleError(err, res);
+
+        res
+          .status(403)
+          .contentType("text/plain")
+          .end("Only .png files are allowed!");
+      });
     }
-})
-var upload = multer({ storage: storage })
-
-/*
-app.use('/a',express.static('/b'));
-Above line would serve all files/folders inside of the 'b' directory
-And make them accessible through http://localhost:3000/a.
-*/
-app.use(express.static(__dirname + '/public'));
-app.use('/images', express.static('images'));
-
-app.post('/profile-upload-single', upload.single('profile-file'), function (req, res, next) {
-  // req.file is the `profile-file` file
-  // req.body will hold the text fields, if there were any
-  console.log(JSON.stringify(req.file))
-  var response = '<a href="/">Home</a><br>'
-  response += "Files uploaded successfully.<br>"
-  response += `<img src="${req.file.path}" /><br>`
-  return res.send(response)
-})
-
-app.post('/profile-upload-multiple', upload.array('profile-files', 12), function (req, res, next) {
-    // req.files is array of `profile-files` files
-    // req.body will contain the text fields, if there were any
-    var response = '<a href="/">Home</a><br>'
-    response += "Files uploaded successfully.<br>"
-    for(var i=0;i<req.files.length;i++){
-        response += `<img src="${req.files[i].path}" /><br>`
-    }
-    
-    return res.send(response)
-})
-   
-
-app.listen(port,() => console.log(`Server running on port ${port}!`))
+  }
+);
 
 module.exports = router;
     
